ed5aa794fc3c948fa1c686b196abdd20dd6199f9
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-10-19 12:37:04 -0300

    Initial commit

---
+++ Health_System/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Health_System
+{
+	class Program
+	{
+		static int health;
+		static int damage;
+		static int healthRegen;
+		static int shield;
+
+
+		static void ShowHUD()
+		{
+			Console.WriteLine("#################################################");
+
+		}
+
+
+		static void Main(string[] args)
+		{
+		}
+	}
+}
